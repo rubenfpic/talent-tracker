f57c4ff61cb5d37ae1bdfbf2daa5a890cd7c5117
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-01-15 11:29:37 +0100

    Autofocus directive fix

--- src/app/features/candidates/components/candidate-list.component.ts
+++ src/app/features/candidates/components/candidate-list.component.ts
@@ -7,11 +7,12 @@ import { combineLatest, debounceTime, distinctUntilChanged, map, startWith } fro
 import { CandidateService } from '../data/candidate.service';
 import { Candidate } from '../models/candidate.model';
 import { InitialsPipe } from '../pipes/initials.pipe';
+import { AutofocusDirective } from '@app/shared/directives/autofocus.directive';
 
 @Component({
   selector: 'app-candidate-list',
   standalone: true,
-  imports: [AsyncPipe, ReactiveFormsModule, RouterLink, TranslateModule, InitialsPipe],
+  imports: [AsyncPipe, ReactiveFormsModule, RouterLink, TranslateModule, InitialsPipe, AutofocusDirective],
   templateUrl: './candidate-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
