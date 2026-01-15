c0a80418da53568397b0d9c32f54b1111cd43700
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-01-15 18:04:01 +0100

    Avatar component test

---
+++ src/app/features/candidates/components/candidate-avatar.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CandidateAvatar } from './candidate-avatar';
+
+describe('CandidateAvatar', () => {
+  let component: CandidateAvatar;
+  let fixture: ComponentFixture<CandidateAvatar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CandidateAvatar]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(CandidateAvatar);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('renders initials when showAvatar is false', () => {
+    // ARRANGE
+    component.name = 'John Doe';
+    component.showAvatar = false;
+    //ACT
+    fixture.detectChanges();
+    const span = fixture.nativeElement.querySelector('.avatar-placeholder span');
+    //ASSERT
+    expect(span?.textContent?.trim()).toBe('JD');
+  });
+});
