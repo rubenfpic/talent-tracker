27984435df6a8bc1216b2062613f8777711325b7
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-02-05 12:20:40 +0100

    Tests fixes

--- src/app/features/offers/components/offer-detail.component.spec.ts
+++ src/app/features/offers/components/offer-detail.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { OfferDetailComponent } from './offer-detail.component';
+import { of } from 'rxjs';
 
 describe('OfferDetailComponent', () => {
   let component: OfferDetailComponent;
@@ -8,7 +10,22 @@ describe('OfferDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OfferDetailComponent]
+      imports: [OfferDetailComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: { paramMap: of(convertToParamMap({ id: '201' })) } },
+        {
+          provide: TranslateService,
+          useValue: {
+            instant: (key: string) => key,
+            get: (key: string) => of(key),
+            onTranslationChange: of({}),
+            onLangChange: of({}),
+            onDefaultLangChange: of({}),
+            onFallbackLangChange: of({}),
+            currentLang: 'en'
+          }
+        }
+      ]
     })
     .compileComponents();
 
--- src/app/features/offers/components/offer-list.component.spec.ts
+++ src/app/features/offers/components/offer-list.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 import { OfferListComponent } from './offer-list.component';
+import { OfferService } from '../data/offer.service';
 
 describe('OfferListComponent', () => {
   let component: OfferListComponent;
@@ -8,7 +10,22 @@ describe('OfferListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OfferListComponent]
+      imports: [OfferListComponent],
+      providers: [
+        { provide: OfferService, useValue: { offers$: of([]) } },
+        {
+          provide: TranslateService,
+          useValue: {
+            instant: (key: string) => key,
+            get: (key: string) => of(key),
+            onTranslationChange: of({}),
+            onLangChange: of({}),
+            onDefaultLangChange: of({}),
+            onFallbackLangChange: of({}),
+            currentLang: 'en'
+          }
+        }
+      ]
     })
     .compileComponents();
 
