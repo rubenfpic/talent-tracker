e5b259ec20fd68a80f9cfb52e72d8a9eeb23cb71
Rubén Fernández Pérez <ruben@MacBook-Air-de-Ruben.local>
2026-02-08 12:32:00 +0100

    Show candidate matches on offer detail

--- src/app/features/offers/components/offer-detail.component.ts
+++ src/app/features/offers/components/offer-detail.component.ts
@@ -2,9 +2,9 @@ import { Component, inject } from '@angular/core';
 import { OfferService } from '../data/offer.service';
 import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { combineLatest, filter, map, switchMap } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { rankCandidatesForOffer } from '@app/shared/utils/candidate-offer-matching';
 
 @Component({
   selector: 'app-offer-detail',
@@ -15,6 +15,13 @@ export class OfferDetailComponent {
   private readonly offerService = inject(OfferService);
   private readonly activatedRoute = inject(ActivatedRoute);
 
-  readonly offer$ = this.activatedRoute.paramMap.pipe(switchMap((params) => this.offerService.getById(Number(params.get('id')))));
-
+  readonly offer$ = this.activatedRoute.paramMap.pipe(
+    switchMap((params) => this.offerService.getById(Number(params.get('id')))),
+  );
+  readonly candidates$ = this.activatedRoute.data.pipe(
+    map((data) => data['candidates'])
+  );
+  readonly matches$ = combineLatest([this.offer$, this.candidates$]).pipe(
+    map(([offer, candidates]) => offer ? rankCandidatesForOffer(candidates, offer).filter(scoreMatch => scoreMatch.score.totalScore > 0) : [])
+  );
 }
